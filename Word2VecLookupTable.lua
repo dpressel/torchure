15243b1a1a761725e325ba38c24e9fdce6cb87c1
dpressel <dpressel@gmail.com>
2016-04-25 09:23:28 -0400

    Support whitelist to limit memory usage and create a padding vector of zeros

--- Word2VecLookupTable.lua
+++ Word2VecLookupTable.lua
@@ -2,22 +2,32 @@ local nn = require 'nn'
 local Word2VecLookupTable, parent = torch.class('Word2VecLookupTable', 'nn.LookupTable')
 
 
-function Word2VecLookupTable:__init(filename)
+function Word2VecLookupTable:__init(filename, whitelist)
     parent.__init(self, 0, 0)
     file = torch.DiskFile(filename, 'r')
     file:binary()
     -- |V| x d
     local vxd = readupto(file, '\n'):split(' ')
-    self.vsz = tonumber(vxd[1])
+    self.vsz = 0
+    local vsz = tonumber(vxd[1])
+    if whitelist then
+       for k,v in pairs(whitelist) do
+	  self.vsz = self.vsz + 1
+       end
+    else
+       self.vsz = vsz
+    end
+
     self.dsz = tonumber(vxd[2])
-    self.weight = torch.FloatTensor(self.vsz+1, self.dsz)
+    self.weight = torch.FloatTensor(self.vsz+1, self.dsz):zero()
     self.gradWeight = torch.FloatTensor(self.vsz+1, self.dsz):zero()
     
     -- loop |V| times read in vocab to table
     rv = {}
     self.vocab = {}
-
-    for i=1,self.vsz do
+    
+    local k = 1
+    for i=1,vsz do
 
        -- read in word       
        local word = readupto(file, ' ')
@@ -25,12 +35,15 @@ function Word2VecLookupTable:__init(filename)
        -- read its vec
        vec = torch.FloatTensor(file:readFloat(self.dsz))
 
-       self.vocab[word] = i
+       if whitelist == nil or whitelist[word] then
+	  self.vocab[word] = k
+	  -- normalize the vector!
+	  self.weight[{{k},{}}] = vec:div(vec:norm())
+	  k = k + 1
 
-       -- normalize the vector!
-       self.weight[{{i},{}}] = vec:div(vec:norm())
+       end
     end
-    
+
     self.vsz = self.vsz + 1
     self.vocab["<PADDING>"] = self.vsz
     file:close()
